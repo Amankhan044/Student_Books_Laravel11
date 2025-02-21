1d5f7e141c4c51cad8156d034930792372e74a26
amankhan044 <amankhan06a@gmail.com>
2025-02-21 06:28:22 +0500

    team author added

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -217,6 +217,22 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
-
+<script>
+$(document).ready(function() {
+    $('#books-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('admin.books.data') }}",
+        columns: [
+            { data: 'id', name: 'id' },
+            { data: 'title', name: 'title' },
+            { data: 'author', name: 'author' },
+            { data: 'category', name: 'category' },
+            { data: 'price', name: 'price' },
+            { data: 'action', name: 'action', orderable: false, searchable: false }
+        ]
+    });
+});
+</script>
 </body>
 </html>
